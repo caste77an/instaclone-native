1d774edf1ec5bdfdac6fec068dee8d782cd2939f
castle <castle@castleui-iMac.local>
2021-10-06 15:21:06 +0900

    #13.6 AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import AppLoading from "./node_modules/expo-app-loading/build/AppLoading";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    console.log(fontPromises);
+    return promise.all(fontPromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
