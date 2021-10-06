819907c08bb4c04187dd4cfe0d1d9b5b806c4788
castle <castle@castleui-iMac.local>
2021-10-06 21:46:09 +0900

    #14.1 Moving Through Screens

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function LogIn() {
+export default function LogIn({ navigation }) {
   return (
     <View>
       <Text>LogIn</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to Create Account</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
